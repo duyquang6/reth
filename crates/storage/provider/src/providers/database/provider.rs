acffd972a8d8296babcfb4e31674c25c058d8170
ligt <me@ligt.dev>
2025-11-14 22:10:06 +0700

    feat: improve write batch trie updates

--- crates/storage/provider/src/providers/database/provider.rs
+++ crates/storage/provider/src/providers/database/provider.rs
@@ -83,7 +83,7 @@ use std::{
     collections::{BTreeMap, BTreeSet},
     fmt::Debug,
     ops::{Deref, DerefMut, Range, RangeBounds, RangeFrom, RangeInclusive},
-    sync::Arc,
+    sync::Arc, time::{Duration, Instant},
 };
 use tracing::{debug, trace};
 
@@ -307,6 +307,7 @@ impl<TX: DbTx + DbTxMut + 'static, N: NodeTypesForProvider> DatabaseProvider<TX,
         debug!(target: "providers::db", block_count = %blocks.len(), "Writing blocks and execution data to storage");
 
         let mut aggregated_hashed_state = HashedPostState::default();
+        let mut aggregated_trie_updates_sorted = TrieUpdatesSorted::default();
 
         // TODO: Do performant / batched writes for each type of object
         // instead of a loop over all blocks,
@@ -314,7 +315,7 @@ impl<TX: DbTx + DbTxMut + 'static, N: NodeTypesForProvider> DatabaseProvider<TX,
         //  * blocks
         //  * state
         //  * hashed state (already done)
-        //  * trie updates (cannot naively extend, need helper)
+        //  * trie updates sorted (already done)
         //  * indices (already done basically)
         // Insert the blocks
         for ExecutedBlock { recovered_block, execution_output, hashed_state, trie_updates } in
@@ -327,18 +328,22 @@ impl<TX: DbTx + DbTxMut + 'static, N: NodeTypesForProvider> DatabaseProvider<TX,
             // Must be written after blocks because of the receipt lookup.
             self.write_state(&execution_output, OriginalValuesKnown::No)?;
 
-            aggregated_hashed_state.extend(Arc::unwrap_or_clone(hashed_state));
+            self.write_hashed_state(&Arc::unwrap_or_clone(hashed_state).into_sorted())?;
+            // aggregated_hashed_state.extend(Arc::unwrap_or_clone(hashed_state));
 
             // sort trie updates and insert changesets
             let trie_updates_sorted = (*trie_updates).clone().into_sorted();
             self.write_trie_changesets(block_number, &trie_updates_sorted, None)?;
-            self.write_trie_updates_sorted(&trie_updates_sorted)?;
+            aggregated_trie_updates_sorted.extend_ref(&trie_updates_sorted);
         }
 
         // batch insert hashes
         if !aggregated_hashed_state.is_empty() {
             self.write_hashed_state(&aggregated_hashed_state.into_sorted())?;
         }
+
+        // batch write trie updates sorted
+        self.write_trie_updates_sorted(&aggregated_trie_updates_sorted)?;
 
         // update history indices
         self.update_history_indices(first_number..=last_block_number)?;
